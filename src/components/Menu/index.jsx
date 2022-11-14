c8f7176675f6015ad111e4e697a216daf5eb4203
Kudi User <kudiuser@Kudis-MacBook-Pro.local>
2022-11-14 10:44:06 +0100

    fix: navigate to place to stay

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -4,7 +4,6 @@ import { XIcon } from "../../assets/svg";
 import { ModalComponent } from "../Modal";
 
 export const Menu = ({ cancel, onClick, show, onHide }) => {
-    
   return (
     <div className="position-fixed top-0 start-0 end-0 bottom-0 zindex ">
       <XIcon onClick={cancel} className="position-absolute x-icon" />
@@ -12,23 +11,25 @@ export const Menu = ({ cancel, onClick, show, onHide }) => {
         <Col className="bg-light ps-4 pt-4">
           <ul className="ps-0">
             <li className="mb-2">
-              <Link to={"/"} className="text-decoration-none text-dark"> Home</Link>
+              <Link to={"/"} className="text-decoration-none text-dark">
+                {" "}
+                Home
+              </Link>
             </li>
-            <li className="mb-2">Place to stay</li>
+            <li className="mb-2">
+              <Link to={"/place-to-stay"}> Place to stay</Link>
+            </li>
             <li className="mb-2">NFTs</li>
             <li className="mb-2">Community</li>
           </ul>
-          <Button
-            className="header-btn shadow-none "
-            onClick={onClick}
-          >
+          <Button className="header-btn shadow-none " onClick={onClick}>
             {" "}
             Connect wallet
           </Button>
         </Col>
-        <Col className="backdrop " onClick={cancel}/>
+        <Col className="backdrop " onClick={cancel} />
       </Row>
-      <ModalComponent show={show} onHide={onHide}/>
+      <ModalComponent show={show} onHide={onHide} />
     </div>
   );
 };
